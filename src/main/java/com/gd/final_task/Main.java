488fe27dd9f7d4840cb7971b842f9dc5d575a53e
ihomziak <homziaki@ae.com>
2022-10-14 22:06:43 +0300

    Modify method's

--- src/main/java/com/gd/final_task/Main.java
+++ src/main/java/com/gd/final_task/Main.java
@@ -1,13 +1,33 @@
 package com.gd.final_task;
 
+import com.gd.final_task.functionality.ChooseCourse;
+import com.gd.final_task.functionality.Functionality;
+import com.gd.final_task.functionality.Parse;
 import com.gd.final_task.io.Input;
-
-import static com.gd.final_task.utils.StartProgram.startProgram;
+import com.gd.final_task.io.Output;
 
 public class Main extends Input {
 
     public static void main(String[] args) {
 
         startProgram();
+    }
+
+    public static void startProgram() {
+        Input input = new Input();
+        Output display = new Output();
+        Functionality calculations = new Functionality();
+        ChooseCourse chooseCourse = new ChooseCourse();
+        Parse parse = new Parse();
+
+        input.inputStudentName();
+        display.displayAvailableCourses();
+        String courseName = chooseCourse.chosenCourse(input.inputCourseName());
+        display.displayChosenCourseName(courseName);
+        display.displayCourseData(courseName);
+        display.displayCourseDuration(courseName);
+        int days = (int) calculations.courseDurationInDays(calculations.courseDurationHours(courseName));
+        String startDate = input.inputStartDate();
+        System.out.println(calculations.addDaysSkippingWeekends(parse.parseStringToDate(startDate), days));
     }
 }
